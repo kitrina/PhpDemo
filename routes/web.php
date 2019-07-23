cff84afd2c6a90dc64542ee28083a6d93f7a541d
huangdd <huangdd@365hrbp.com>
2019-07-23 18:26:51 +0800

    配置修改提交

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,41 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/get-demo', function () {
+    getParams();
+});
+
+Route::post('/post-demo', function () {
+    postParams();
+});
+
+Route::post('/post-json', function () {
+    postJson();
+});
+
+function getParams()
+{
+    $id = $_GET['id'];
+    $name = $_GET['name'];
+
+    echo $id . ' ' . $name;
+}
+
+
+function postParams() {
+    $first = $_GET['id'];
+    $second = $_GET['name'];
+
+    echo $first . ' ' . $second;
+}
+
+function postJson() {
+    $re = file_get_contents("php://input");
+    $reArr = json_decode($re, true);
+
+    $first = $reArr['id'];
+    $second = $reArr['name'];
+
+    echo $first . ' ' . $second;
+}
