b17a88b3f4b94592201fa16ca1130d0d169cbf2e
huangdd <huangdd@365hrbp.com>
2019-07-25 12:55:11 +0800

    模型的新建与调用

--- app/Http/Controllers/MemberController.php
+++ app/Http/Controllers/MemberController.php
@@ -2,16 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Member;
+
 class MemberController extends Controller
 {
     public function info($id)
     {
+        return Member::getMember();
+
 //        return 'member-info-id-' . $id;
 //        return route('memberinfo');
 //        return view('member-info');
-        return view('member/info', [
-            'name' => '小阿飞',
-            'age' => 18
-        ]);
+//        return view('member/info', [
+//            'name' => '小阿飞',
+//            'age' => 18
+//        ]);
     }
 }
