3be7d232e72abe80b1701914f3de0713598445a5
huangdd <huangdd@365hrbp.com>
2019-11-18 18:25:49 +0800

    配置文件忽略

--- routes/blog.php
+++ routes/blog.php
@@ -2,7 +2,7 @@
 /*
  * 博客项目接口
  */
-Route::any('/app/test', ['uses' => 'Blog\AppController@test']);
-Route::any('/app/add', ['uses' => 'Blog\AppController@add']);
-Route::any('/app/query', ['uses' => 'Blog\AppController@query']);
-Route::any('/app/clear', ['uses' => 'Blog\AppController@clear']);
+Route::any('/test', ['uses' => 'Blog\AppController@test']);
+Route::any('/add', ['uses' => 'Blog\AppController@add']);
+Route::any('/query', ['uses' => 'Blog\AppController@query']);
+Route::any('/clear', ['uses' => 'Blog\AppController@clear']);
