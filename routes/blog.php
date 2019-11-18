6242e44d6a6c24e2e26da0d3ee0534199d8f2d99
huangdd <huangdd@365hrbp.com>
2019-11-18 16:39:52 +0800

    测试接口添加

--- routes/blog.php
+++ routes/blog.php
@@ -2,6 +2,7 @@
 /*
  * 博客项目接口
  */
+Route::any('/app/test', ['uses' => 'Blog\AppController@test']);
 Route::any('/app/add', ['uses' => 'Blog\AppController@add']);
 Route::any('/app/query', ['uses' => 'Blog\AppController@query']);
 Route::any('/app/clear', ['uses' => 'Blog\AppController@clear']);
